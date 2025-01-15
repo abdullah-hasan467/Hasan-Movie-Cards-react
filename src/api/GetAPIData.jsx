cecab9684e0396d8ed0da06f179811dc6befe9ba
Md. Abdullah Al Hasan <hasan.cse.iub@gmail.com>
2025-01-15 19:57:47 +0600

    ppp

--- src/api/GetAPIData.jsx
+++ src/api/GetAPIData.jsx
@@ -6,17 +6,17 @@ export const getMoviesData = async () => {
       // `http://www.omdbapi.com/?apikey=${import.meta.env.VITE_API_KEY}&s=marvel&page=1`
 
       // For Spiderman
-       `https://www.omdbapi.com/?apikey=${import.meta.env.VITE_API_KEY}&s=spider-man&page=1`
+       `http://www.omdbapi.com/?apikey=${import.meta.env.VITE_API_KEY}&s=spider-man&page=1`
 
        //For Avenger
-    //     `https://www.omdbapi.com/?apikey=${import.meta.env.VITE_API_KEY}&s=avengers&page=1`
+    //     `http://www.omdbapi.com/?apikey=${import.meta.env.VITE_API_KEY}&s=avengers&page=1`
 
     // for Animation movies
-    //  `https://www.omdbapi.com/?apikey=${import.meta.env.VITE_API_KEY}&s=animation&page=1`
+    //  `http://www.omdbapi.com/?apikey=${import.meta.env.VITE_API_KEY}&s=animation&page=1`
 
 
   //    // For popular Movies
-  // `https://www.omdbapi.com/?apikey=${import.meta.env.VITE_API_KEY}&s=hollywood&page=1`
+  // `http://www.omdbapi.com/?apikey=${import.meta.env.VITE_API_KEY}&s=hollywood&page=1`
 
 
 
